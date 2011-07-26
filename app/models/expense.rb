0eeeaebb289edd70d2f5a304c3f8456b1e865c58
Chris Yuska <chrisyuska@gmail.com>
2011-07-26 15:45:15 -0400

    Fixed ability to edit expense traits

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,8 @@
 class Expense < ActiveRecord::Base
   belongs_to :user, :foreign_key => "created_by"
 
-  validates_presence_of :category, :amount, :created_by
+  attr_accessible :cost, :comments, :category, :amount, :paid
+
+  validates_presence_of :category, :amount, :created_by, :comments
   validates :amount, :numericality => true
 end
